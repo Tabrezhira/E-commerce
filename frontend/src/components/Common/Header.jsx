f947bc93560d59530acd19a3ed98c162dc2a3bf0
TabrezHira <51405264+Tabrezhira@users.noreply.github.com>
2025-03-02 01:05:39 +0530

    Top Bar & Layout

---
+++ frontend/src/components/Common/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Topbar from '../Layout/Topbar'
+
+function Header() {
+  return (
+    <div>
+        {/* Topbar */}
+        <Topbar/>
+        {/* navbar */}
+        {/* Cart Drawer */}
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,14 @@
+import Header from '../Common/Header'
+
+function UserLayout() {
+  return (
+    <>
+    {/* {Header} */}
+    <Header/>
+    {/* {Main content} */}
+    {/* {Footer} */}
+    </>
+  )
+}
+
+export default UserLayout
