124e01844ebbf975de3f47163cf6c05934e2c8b7
TabrezHira <51405264+Tabrezhira@users.noreply.github.com>
2025-03-02 19:30:54 +0530

    Day-2 Cart

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import { IoMdClose } from "react-icons/io";
+import CartContents from '../Cart/CartContents';
 
 function CartDrawer({drawerOpen, toggleCartDrawer}) {
 
     return (
-    <div className={` fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-1/4 h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+    <div className={` fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? 'translate-x-0' : 'translate-x-full'}`}>
         {/* Close Button */}
         <div className='flex justify-end p-4'>
             <button onClick={toggleCartDrawer}>
@@ -15,6 +16,7 @@ function CartDrawer({drawerOpen, toggleCartDrawer}) {
         <div className='flex-grow p-4 overflow-y-auto'>
             <h2 className='text-xl font-semibold mb-4'> Your Cart</h2>
             {/* Component for Cart Contents */}
+            <CartContents/>
         </div>
 
         {/* Checkout button fixed at the bottom */}
