de771a1858313acc18450d9e2e114e85c49694bb
TabrezHira <51405264+Tabrezhira@users.noreply.github.com>
2025-03-04 02:53:57 +0530

    You May Also Like

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,58 @@ import Hero from '../components/Layout/Hero'
 import GenderCollectionSection from '../components/Products/GenderCollectionSection'
 import NewArrivals from '../components/Products/NewArrivals'
 import ProductDetails from '../components/Products/ProductDetails'
+import ProductsGrid from '../components/Products/ProductsGrid'
+
+const placeholderProducts = [
+  {
+    _id:1,
+    name:"Product 1",
+    price:100,
+    images:[{url:'https://picsum.photos/500/500?random=7'}]
+},
+{
+    _id:2,
+    name:"Product 2",
+    price:100,
+    images:[{url:'https://picsum.photos/500/500?random=8'}]
+},
+{
+    _id:3,
+    name:"Product 3",
+    price:100,
+    images:[{url:'https://picsum.photos/500/500?random=9'}]
+},
+{
+    _id:4,
+    name:"Product 4",
+    price:100,
+    images:[{url:'https://picsum.photos/500/500?random=10'}]
+},
+{
+  _id:5,
+  name:"Product 1",
+  price:100,
+  images:[{url:'https://picsum.photos/500/500?random=11'}]
+},
+{
+  _id:6,
+  name:"Product 2",
+  price:100,
+  images:[{url:'https://picsum.photos/500/500?random=12'}]
+},
+{
+  _id:7,
+  name:"Product 3",
+  price:100,
+  images:[{url:'https://picsum.photos/500/500?random=13'}]
+},
+{
+  _id:8,
+  name:"Product 4",
+  price:100,
+  images:[{url:'https://picsum.photos/500/500?random=14'}]
+}
+]
 
 function Home() {
   return (
@@ -16,6 +68,11 @@ function Home() {
           Best Seller
         </h2>
         <ProductDetails/>
+
+        <div className=' container mx-auto'>
+          <h2 className='text-3xl text-center font-bold mb-4'> Top Wears for Women</h2>
+          <ProductsGrid product={placeholderProducts}/>
+        </div>
     </div>
   )
 }
