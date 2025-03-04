21c9e6e37a843f8408417f9057b110caa4f93c56
TabrezHira <51405264+Tabrezhira@users.noreply.github.com>
2025-03-05 05:11:45 +0530

    paypal start

--- frontend/src/components/Cart/PayPalButton.jsx
+++ frontend/src/components/Cart/PayPalButton.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-function PayPalButton() {
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
+
+function PayPalButton({amount, onSuccess, onError}) {
   return (
-    <div>PayPalButton</div>
+    <PayPalScriptProvider options={{ clientId: "ARfRPADUW8sRR5u8FWQIX6psY7luVNpUADj3MH7DNzHvHs18s-tr34Jm1rFPpJDKi7hPL8hiFgPGIX14" }}>
+        <PayPalButtons style={{ layout: "vertical" }} createOrder={(data, actions) => {
+            return actions.order.create({
+                purchase_units:[{amount: {value: amount}}]
+            })
+        }} 
+        onApprove={(data, actions) => {
+            return actions.order.capture().then(onSuccess)
+        }}
+        onError={onError}
+        />
+    </PayPalScriptProvider>
   )
 }
 
