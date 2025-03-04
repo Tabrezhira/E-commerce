043243352c88e5e69e79b71b522c249bc39ee0d6
TabrezHira <51405264+Tabrezhira@users.noreply.github.com>
2025-03-04 19:25:04 +0530

    Day-4

---
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+
+function MyOrdersPage() {
+    const [orders, setOrders] = useState([])
+
+    useEffect(() => {
+        //Simulate fetching orders
+       setTimeout(() => {
+
+       }) 
+    })
+    return (
+    <div>MyOrdersPage</div>
+  )
+}
+
+export default MyOrdersPage
