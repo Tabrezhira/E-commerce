6ab2359371b46113b1e2179c24a15882090d9439
TabrezHira <51405264+Tabrezhira@users.noreply.github.com>
2025-03-07 04:24:41 +0530

    user

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,29 @@ const cors = require('cors')
 const dotenv = require('dotenv')
 const app = express()
 const connectDB = require('./config/db')
+const userRoutes = require('./routes/userRoutes')
 app.use(express.json())
 app.use(cors())
 
 dotenv.config()
-
-
-
 const Port = process.env.PORT || 3000;
-
 // Connect 
 connectDB()
+
+
+
+
 
 app.get('/', (req,res) => {
     res.send('Welcome to Rabbit API!')
 })
+
+
+// API Routes
+
+app.use('/api/users', userRoutes)
+
+
 
 app.listen(Port,()=>{
     console.log(`Server is running on http://localhost:${Port}`)
