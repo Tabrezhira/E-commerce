3caca03457f86070adbd35e6e4bcb39ed75479af
TabrezHira <51405264+Tabrezhira@users.noreply.github.com>
2025-03-11 05:11:37 +0530

    collection page
    half filter is working half we need to fix

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -26,10 +26,10 @@ function Navbar() {
 
         {/*  Center - Navigation Link */}
         <div className=' hidden md:flex space-x-6'>
-            <Link to='/collections/all' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Men</Link>
-            <Link to='#' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>WoMen</Link>
-            <Link to='#' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Top wear</Link>
-            <Link to='#' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>bottom wear</Link>
+            <Link to='/collections/all?gender=Men' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Men</Link>
+            <Link to='/collections/all?gender=Women' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>WoMen</Link>
+            <Link to='/collections/all?category=Top+Wear' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Top wear</Link>
+            <Link to='/collections/all?category=Bottom+Wear' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>bottom wear</Link>
         </div>
 
         {/* Right - Icons */}
@@ -64,16 +64,16 @@ function Navbar() {
             <div className='p-4 '>
                 <h2 className='text-xl font-semibold mb-4'>Menu</h2>
                 <nav className=' space-y-4'>
-                    <Link to="#" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
+                    <Link to="/collections/all?gender=Men" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
                         Men
                     </Link>
-                    <Link to="#" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
+                    <Link to="/collections/all?gender=Women" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
                         Women
                     </Link>
-                    <Link to="#" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
+                    <Link to="/collections/all?category=Top+Wear" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
                         Top Wear
                     </Link>
-                    <Link to="#" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
+                    <Link to="/collections/all?category=Bottom+Wear" onClick={toggleNavDrawer} className='block text-gray-600 hover:text-black'>
                         Bottom Wear
                     </Link>
                 </nav>
--- frontend/src/pages/CollectionPage.jsx
+++ frontend/src/pages/CollectionPage.jsx
@@ -3,12 +3,24 @@ import { FaFilter } from "react-icons/fa";
 import FilterSidebar from '../components/Products/FilterSidebar';
 import SortOptions from '../components/Products/SortOptions';
 import ProductsGrid from '../components/Products/ProductsGrid';
+import { useParams, useSearchParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProductsByFilters } from '../../redux/slices/productSlice';
 
 
 function CollectionPage() {
-    const [products, setProducts] = useState([])
+    const {collection} = useParams()
+    const [searchParams] = useSearchParams();
+    const dispatch = useDispatch()
+    const {products, loading, error} = useSelector((state) => state.products)
+    const queryParams = Object.fromEntries([...searchParams])
+    // const [products, setProducts] = useState([])
     const sidebarRef =useRef(null) 
     const [isSidebarOpen, setSidebarOpen] = useState(false)
+
+    useEffect(() =>{
+        dispatch(fetchProductsByFilters({collection, ...queryParams}))
+    },[dispatch, collection, searchParams])
 
     const toggleSidebar = () => {
         setSidebarOpen(!isSidebarOpen)
@@ -33,61 +45,61 @@ function CollectionPage() {
 
     },[])
 
-    useEffect(() => {
-        setTimeout(() => {
-            const fetchedProducts = [
-                {
-                    _id:1,
-                    name:"Product 1",
-                    price:100,
-                    images:[{url:'https://picsum.photos/500/500?random=7'}]
-                },
-                {
-                    _id:2,
-                    name:"Product 2",
-                    price:100,
-                    images:[{url:'https://picsum.photos/500/500?random=8'}]
-                },
-                {
-                    _id:3,
-                    name:"Product 3",
-                    price:100,
-                    images:[{url:'https://picsum.photos/500/500?random=9'}]
-                },
-                {
-                    _id:4,
-                    name:"Product 4",
-                    price:100,
-                    images:[{url:'https://picsum.photos/500/500?random=10'}]
-                },
-                {
-                  _id:5,
-                  name:"Product 1",
-                  price:100,
-                  images:[{url:'https://picsum.photos/500/500?random=11'}]
-                },
-                {
-                  _id:6,
-                  name:"Product 2",
-                  price:100,
-                  images:[{url:'https://picsum.photos/500/500?random=12'}]
-                },
-                {
-                  _id:7,
-                  name:"Product 3",
-                  price:100,
-                  images:[{url:'https://picsum.photos/500/500?random=13'}]
-                },
-                {
-                  _id:8,
-                  name:"Product 4",
-                  price:100,
-                  images:[{url:'https://picsum.photos/500/500?random=14'}]
-                }
-            ];
-            setProducts(fetchedProducts)
-        })
-    },[])
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         const fetchedProducts = [
+    //             {
+    //                 _id:1,
+    //                 name:"Product 1",
+    //                 price:100,
+    //                 images:[{url:'https://picsum.photos/500/500?random=7'}]
+    //             },
+    //             {
+    //                 _id:2,
+    //                 name:"Product 2",
+    //                 price:100,
+    //                 images:[{url:'https://picsum.photos/500/500?random=8'}]
+    //             },
+    //             {
+    //                 _id:3,
+    //                 name:"Product 3",
+    //                 price:100,
+    //                 images:[{url:'https://picsum.photos/500/500?random=9'}]
+    //             },
+    //             {
+    //                 _id:4,
+    //                 name:"Product 4",
+    //                 price:100,
+    //                 images:[{url:'https://picsum.photos/500/500?random=10'}]
+    //             },
+    //             {
+    //               _id:5,
+    //               name:"Product 1",
+    //               price:100,
+    //               images:[{url:'https://picsum.photos/500/500?random=11'}]
+    //             },
+    //             {
+    //               _id:6,
+    //               name:"Product 2",
+    //               price:100,
+    //               images:[{url:'https://picsum.photos/500/500?random=12'}]
+    //             },
+    //             {
+    //               _id:7,
+    //               name:"Product 3",
+    //               price:100,
+    //               images:[{url:'https://picsum.photos/500/500?random=13'}]
+    //             },
+    //             {
+    //               _id:8,
+    //               name:"Product 4",
+    //               price:100,
+    //               images:[{url:'https://picsum.photos/500/500?random=14'}]
+    //             }
+    //         ];
+    //         setProducts(fetchedProducts)
+    //     })
+    // },[])
   return (
     <div className='flex flex-col lg:flex-row'>
         {/* {Mobile Filter button} */}
@@ -106,7 +118,7 @@ function CollectionPage() {
              {/* Sort options */}
              <SortOptions/>
              {/* Product Grid */}
-             <ProductsGrid product={products}/>
+             <ProductsGrid products={products} loading={loading} error={error}/>
         </div>
     </div>
   )
